244235a9e0bc868a5506902d8228890f7424b3a0
ClearFive <2217350445@qq.com>
2016-12-20 16:06:33 +0800

    SearchContentService

---
+++ src/main/java/cn/edu/bjtu/weibo/service/impl/SearchContentServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.edu.bjtu.weibo.service.impl;
+
+import java.util.List;
+
+import cn.edu.bjtu.weibo.model.BaseContentSR;
+import cn.edu.bjtu.weibo.service.SearchContentService;
+
+public class SearchContentServiceImpl implements SearchContentService {
+
+	@Override
+	public List<BaseContentSR> getSearchedWeiboList(String keyword, int pageIndex, int numberPerPage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
